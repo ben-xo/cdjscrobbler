2dbcc8020c2145b3c2a4f3e806aa16504f7f7284
Ben XO <ben@BigMac-6.local>
2019-09-23 22:02:14 +0100

    That looks like a fully fleshed out State Machine to me.

--- src/main/java/am/xo/cdjscrobbler/SongModel.java
+++ src/main/java/am/xo/cdjscrobbler/SongModel.java
@@ -1,44 +1,44 @@
 package am.xo.cdjscrobbler;
 
 import am.xo.cdjscrobbler.SongEvents.NowPlayingEvent;
+import am.xo.cdjscrobbler.SongEvents.ResetEvent;
+import am.xo.cdjscrobbler.SongEvents.ScrobbleEvent;
+import am.xo.cdjscrobbler.SongEvents.TransitionEvent;
 import org.deepsymmetry.beatlink.CdjStatus;
 import org.deepsymmetry.beatlink.Util;
+
+import java.util.ArrayList;
 
 public class SongModel {
 
         /**
          * Song Transitions
-         * 	Started -> Ignored (happens when no metadata available)
          * 	Started -> Cueing ✅
-         * 	Ignored -> Stopped
          * 	Cueing -> Started ✅
          * 	Cueing -> Playing ✅              <- raise a now playing (get metadata now)
-         * 	Playing -> PlayingPaused
-         * 	Playing -> Stopped
-         * 	Playing -> Scrobbling
-         * 	PlayingPaused -> Playing
-         * 	PlayingPaused -> Stopped
-         * 	Scrobbling -> ScrobblingPaused
-         * 	Scrobbling -> Stopped           <- raise a retire-scrobbling
-         * 	ScrobblingPaused -> Scrobbling
-         * 	ScrobblingPaused -> Stopped     <- raise a retire-scrobbling
+         * 	Playing -> PlayingPaused ✅
+         * 	Playing -> Stopped ✅
+         * 	Playing -> Scrobbling ✅
+         * 	PlayingPaused -> Playing ✅
+         * 	PlayingPaused -> Stopped ✅
+         * 	Scrobbling -> ScrobblingPaused ✅
+         * 	Scrobbling -> Stopped         ✅  <- raise a retire-scrobbling
+         * 	ScrobblingPaused -> Scrobbling ✅
+         * 	ScrobblingPaused -> Stopped    ✅ <- raise a retire-scrobbling
          */
+
+        // TODO: reuse instances of TransitionEvent and ResetEvent
 
     public enum SongState {
         STARTED {
             @Override
             public SongEvent applyNext(SongModel model, CdjStatus update) {
                 if(update.isPlayingForwards()) {
+                    model.rekordboxId = update.getRekordboxId();
                     model.currentState = CUEING;
+                    // don't bother with a transition event. Nobody cares
                 }
                 return null;
-            }
-        },
-
-        IGNORED {
-            @Override
-            public SongEvent applyNext(SongModel model, CdjStatus update) {
-                return SongStateTransition.NONE;
             }
         },
 
@@ -47,16 +47,21 @@ public class SongModel {
             public SongEvent applyNext(SongModel model, CdjStatus update) {
                 model.addPlaytimeFrom(update);
                 if(update.isPlayingForwards()) {
+                    // we'll take whatever it is, at this point.
+                    // we don't try resolve any metadata until after the now playing point anyway
+                    model.rekordboxId = update.getRekordboxId();
                     if(model.isPastNowPlaying()) {
                         model.currentState = PLAYING;
+                        return new NowPlayingEvent();
                     }
-                    return new NowPlayingEvent();
                 } else {
-                    // whilst cueing - defined as being first 30s of play - stopping play resets the model
+                    // whilst cueing - any sort of stopping play resets the model.
+                    // this includes searching, rewinding, or other events.
                     model.resetPlay();
                     model.currentState = STARTED;
-                    return null;
+                    // don't bother with a transition event. Nobody cares
                 }
+                return null;
             }
         },
 
@@ -64,12 +69,17 @@ public class SongModel {
             @Override
             public SongEvent applyNext(SongModel model, CdjStatus update) {
                 model.addPlaytimeFrom(update);
-                if(update.isPlayingForwards()) {
-                    if(model.isPastScrobblePoint()) {
+                if(isStopping(model, update)) {
+                    model.currentState = STOPPED;
+                    return new ResetEvent();
+                } else if(update.isPlayingForwards()) {
+                    if (model.isPastScrobblePoint()) {
                         model.currentState = SCROBBLING;
+                        return new TransitionEvent();
                     }
                 } else {
                     model.currentState = PLAYINGPAUSED;
+                    return new TransitionEvent();
                 }
                 return null;
             }
@@ -78,8 +88,12 @@ public class SongModel {
         PLAYINGPAUSED {
             @Override
             public SongEvent applyNext(SongModel model, CdjStatus update) {
-                if(update.isPlayingForwards()) {
+                if(isStopping(model, update)) {
+                    model.currentState = STOPPED;
+                    return new ResetEvent();
+                } else if(update.isPlayingForwards()) {
                     model.currentState = PLAYING;
+                    return new TransitionEvent();
                 }
                 return null;
             }
@@ -89,8 +103,12 @@ public class SongModel {
             @Override
             public SongEvent applyNext(SongModel model, CdjStatus update) {
                 model.addPlaytimeFrom(update);
-                if(!update.isPlayingForwards()) {
+                if(isStopping(model, update)) {
+                    model.currentState = STOPPED;
+                    return new ScrobbleEvent(model.song);
+                } else if(!update.isPlayingForwards()) {
                     model.currentState = SCROBBLINGPAUSED;
+                    return new TransitionEvent();
                 }
                 return null;
             }
@@ -99,8 +117,12 @@ public class SongModel {
         SCROBBLINGPAUSED {
             @Override
             public SongEvent applyNext(SongModel model, CdjStatus update) {
-                if(update.isPlayingForwards()) {
+                if(isStopping(model, update)) {
+                    model.currentState = STOPPED;
+                    return new ScrobbleEvent(model.song);
+                } else if(update.isPlayingForwards()) {
                     model.currentState = SCROBBLING;
+                    return new TransitionEvent();
                 }
                 return null;
             }
@@ -121,6 +143,10 @@ public class SongModel {
          * @return
          */
         abstract public SongEvent applyNext(SongModel model, CdjStatus update);
+
+        public boolean isStopping(SongModel model, CdjStatus update) {
+            return update.isAtEnd() || !update.isTrackLoaded() || update.getRekordboxId() != model.rekordboxId;
+        }
     }
 
 
@@ -135,25 +161,36 @@ public class SongModel {
      */
 
     protected int deviceNumber = 0;
-    protected boolean hasMetadata = false;
     protected SongDetails song;
     protected long totalPlayTime = 0;
     protected SongState currentState = SongState.STARTED;
     protected long lastUpdate = 0;
+    protected int rekordboxId = 0;
 
     public SongModel(int deviceNumber) {
         this.deviceNumber = deviceNumber;
     }
 
-    public SongEvent update(CdjStatus update) {
-        SongEvent yieldedEvent = currentState.applyNext(this, );
+    public ArrayList<SongEvent> update(CdjStatus update) {
+        return update(update, new ArrayList<SongEvent>);
+    }
+
+    protected ArrayList<SongEvent> update(CdjStatus update, ArrayList<SongEvent> returnedEvents) {
+        SongEvent yieldedEvent = currentState.applyNext(this, update);
         lastUpdate = update.getTimestamp();
-        return yieldedEvent;
+        if(yieldedEvent != null) {
+            // must apply continuously to catch all events in sequence.
+            // this could happen if a single event increments time by a large amount.
+            // (TODO: Yeah, this could be a loop)
+            returnedEvents.add(yieldedEvent);
+            return update(update, returnedEvents);
+        }
+        return returnedEvents;
     }
 
     public void resetPlay() {
-        hasMetadata = false;
         song = null;
+        rekordboxId = 0;
         totalPlayTime = 0;
     }
 
@@ -169,5 +206,8 @@ public class SongModel {
         return totalPlayTime >= 30000000; // 30s
     }
 
-//    public addPlayTime()
+    public boolean isPastScrobblePoint() {
+        return song != null && totalPlayTime > song.getScrobblePointNanos();
+    }
+
 }
