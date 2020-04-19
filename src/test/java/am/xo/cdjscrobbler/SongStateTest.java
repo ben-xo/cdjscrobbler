a7fd585c7d7ca7b87bfdcceb04e371dac35b252e
Ben Isaacs <ben@last.fm>
2020-04-19 15:50:31 +0100

    Finish writing the tests for SongState.

--- src/test/java/am/xo/cdjscrobbler/SongStateTest.java
+++ src/test/java/am/xo/cdjscrobbler/SongStateTest.java
@@ -27,7 +27,7 @@
 
 package am.xo.cdjscrobbler;
 
-import am.xo.cdjscrobbler.SongEvents.NewSongLoadedEvent;
+import am.xo.cdjscrobbler.SongEvents.*;
 import junit.framework.TestCase;
 import org.deepsymmetry.beatlink.CdjStatus;
 import org.junit.Before;
@@ -36,8 +36,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.mockito.Mockito.lenient;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SongStateTest extends TestCase {
@@ -52,28 +51,24 @@ public class SongStateTest extends TestCase {
     public void setup() {
         m.rekordboxId = 100;
         m.currentState = SongState.STARTED;
+        lenient().when(update.isTrackLoaded()).thenReturn(true);
+        lenient().when(update.getRekordboxId()).thenReturn(100);
     }
 
     @Test
     public void test_isStopping_if_at_end() {
         when(update.isAtEnd()).thenReturn(true);
-        lenient().when(update.isTrackLoaded()).thenReturn(true);
-        lenient().when(update.getRekordboxId()).thenReturn(100);
         assertEquals(true, SongState.STOPPED.isStopping(m, update));
     }
 
     @Test
     public void test_isStopping_no_track_loaded() {
-        lenient().when(update.isAtEnd()).thenReturn(false);
         when(update.isTrackLoaded()).thenReturn(false);
-        lenient().when(update.getRekordboxId()).thenReturn(100);
         assertEquals(true, SongState.STOPPED.isStopping(m, update));
     }
 
     @Test
     public void test_isStopping_different_song() {
-        lenient().when(update.isAtEnd()).thenReturn(false);
-        lenient().when(update.isTrackLoaded()).thenReturn(true);
         when(update.getRekordboxId()).thenReturn(999);
         assertEquals(true, SongState.STOPPED.isStopping(m, update));
     }
@@ -120,139 +115,214 @@ public class SongStateTest extends TestCase {
         assertEquals(NewSongLoadedEvent.class, SongState.STARTED.applyNext(m,update).getClass());
     }
 
+    @Test
     public void test_CUEING_adds_playtime_to_model() {
-
+        SongState.CUEING.applyNext(m, update);
+        verify(m, times(1)).addPlaytimeFrom(update);
     }
 
+    @Test
     public void test_CUEING_emits_NowPlayingEvent_when_song_passes_now_playing_point() {
-
+        when(m.isPlayingForward(update)).thenReturn(true);
+        when(m.isPastNowPlaying()).thenReturn(true);
+        assertEquals(NowPlayingEvent.class, SongState.CUEING.applyNext(m, update).getClass());
     }
 
+    @Test
     public void test_CUEING_to_PLAYING_when_song_passes_now_playing_point() {
-
+        when(m.isPlayingForward(update)).thenReturn(true);
+        when(m.isPastNowPlaying()).thenReturn(true);
+        SongState.CUEING.applyNext(m, update);
+        assertEquals(SongState.PLAYING, m.currentState);
     }
 
+    @Test
     public void test_CUEING_to_CUEINGPAUSED_when_a_song_stops_before_now_playing_point() {
-
+        when(m.isPlayingForward(update)).thenReturn(false);
+        SongState.CUEING.applyNext(m, update);
+        assertEquals(SongState.CUEINGPAUSED, m.currentState);
     }
 
+    @Test
     public void test_CUEING_to_CUEINGPAUSED_resets_song_model() {
-
+        when(m.isPlayingForward(update)).thenReturn(false);
+        SongState.CUEING.applyNext(m, update);
+        verify(m).resetPlay();
     }
 
+    @Test
     public void test_CUEING_to_CUEINGPAUSED_emits_no_event() {
-
+        when(m.isPlayingForward(update)).thenReturn(false);
+        assertNull(SongState.CUEING.applyNext(m, update));
     }
 
+    @Test
     public void test_CUEINGPAUSED_to_STOPPED_when_song_is_stopping() {
-
+        when(update.isAtEnd()).thenReturn(true);
+        SongState.CUEINGPAUSED.applyNext(m, update);
+        assertEquals(SongState.STOPPED, m.currentState);
     }
 
+    @Test
     public void test_CUEINGPAUSED_emits_ResetEvent_when_song_is_stopping() {
-
+        when(update.isAtEnd()).thenReturn(true);
+        assertEquals(ResetEvent.class, SongState.CUEINGPAUSED.applyNext(m, update).getClass());
     }
 
+    @Test
     public void test_CUEINGPAUSED_to_CUEING_when_song_is_playing_forward() {
-
+        when(m.isPlayingForward(update)).thenReturn(true);
+        SongState.CUEINGPAUSED.applyNext(m, update);
+        assertEquals(SongState.CUEING, m.currentState);
     }
 
+    @Test
     public void test_PLAYING_adds_playtime_to_model() {
-
+        SongState.PLAYING.applyNext(m, update);
+        verify(m, times(1)).addPlaytimeFrom(update);
     }
 
+    @Test
     public void test_PLAYING_to_STOPPED_when_song_is_stopping() {
-
+        when(update.isAtEnd()).thenReturn(true);
+        SongState.PLAYING.applyNext(m, update);
+        assertEquals(SongState.STOPPED, m.currentState);
     }
 
+    @Test
     public void test_PLAYING_emits_ResetEvent_when_song_is_stopping() {
-
+        when(update.isAtEnd()).thenReturn(true);
+        assertEquals(ResetEvent.class, SongState.PLAYING.applyNext(m, update).getClass());
     }
 
+    @Test
     public void test_PLAYING_to_SCROBBLING_when_song_is_past_scrobble_point() {
-
+        when(m.isPlayingForward(update)).thenReturn(true);
+        when(m.isPastScrobblePoint()).thenReturn(true);
+        SongState.PLAYING.applyNext(m,update);
+        assertEquals(SongState.SCROBBLING, m.currentState);
     }
 
+    @Test
     public void test_PLAYING_emits_TransitionEvent_when_song_is_past_scrobble_point() {
-
+        when(m.isPlayingForward(update)).thenReturn(true);
+        when(m.isPastScrobblePoint()).thenReturn(true);
+        assertEquals(TransitionEvent.class, SongState.PLAYING.applyNext(m, update).getClass());
     }
 
+    @Test
     public void test_PLAYING_to_PLAYINGPAUSED_if_not_stopping_and_not_playing_forward() {
-
+        SongState.PLAYING.applyNext(m, update);
+        assertEquals(SongState.PLAYINGPAUSED, m.currentState);
     }
 
-    public void test_PLAYING_to_PLAYINGPAUSED_emits_no_event() {
-
-    }
-
+    @Test
     public void test_PLAYING_emits_TransitionEvent_if_not_stopping_and_not_playing_forward() {
-
+        assertEquals(TransitionEvent.class, SongState.PLAYING.applyNext(m, update).getClass());
     }
 
+    @Test
     public void test_PLAYING_emit_no_event_if_playing_forward_before_scrobble_point() {
-
+        when(m.isPlayingForward(update)).thenReturn(true);
+        when(m.isPastScrobblePoint()).thenReturn(false);
+        assertNull(SongState.PLAYING.applyNext(m, update));
     }
 
+    @Test
     public void test_PLAYINGPAUSED_to_STOPPED_if_song_is_stopping() {
-
+        when(update.isAtEnd()).thenReturn(true);
+        SongState.PLAYINGPAUSED.applyNext(m, update);
+        assertEquals(SongState.STOPPED, m.currentState);
     }
 
+    @Test
     public void test_PLAYINGPAUSED_to_STOPPED_emits_ResetEvent() {
-
+        when(update.isAtEnd()).thenReturn(true);
+        assertEquals(ResetEvent.class, SongState.PLAYINGPAUSED.applyNext(m, update).getClass());
     }
 
+    @Test
     public void test_PLAYINGPAUSED_to_PLAYING_if_song_is_playing_forward() {
-
+        when(m.isPlayingForward(update)).thenReturn(true);
+        SongState.PLAYINGPAUSED.applyNext(m, update);
+        assertEquals(SongState.PLAYING, m.currentState);
     }
 
+    @Test
     public void test_PLAYINGPAUSED_to_PLAYING_emits_TransitionEvent() {
-
+        when(m.isPlayingForward(update)).thenReturn(true);
+        assertEquals(TransitionEvent.class, SongState.PLAYINGPAUSED.applyNext(m, update).getClass());
     }
 
+    @Test
     public void test_PLAYINGPAUSED_emits_no_event_if_not_stopping_and_not_playing_forward() {
-
+        assertNull(SongState.PLAYINGPAUSED.applyNext(m, update));
     }
 
+    @Test
     public void test_SCROBBLING_adds_playtime_to_model() {
-
+        SongState.SCROBBLING.applyNext(m, update);
+        verify(m, times(1)).addPlaytimeFrom(update);
     }
 
+    @Test
     public void test_SCROBBLING_to_STOPPED_when_song_is_stopping() {
-
+        when(update.isAtEnd()).thenReturn(true);
+        SongState.SCROBBLING.applyNext(m, update);
+        assertEquals(SongState.STOPPED, m.currentState);
     }
 
+    @Test
     public void test_SCROBBLING_emits_ScrobbleEvent_when_song_is_stopping() {
-
+        when(update.isAtEnd()).thenReturn(true);
+        assertEquals(ScrobbleEvent.class, SongState.SCROBBLING.applyNext(m, update).getClass());
     }
 
+    @Test
     public void test_SCROBBLING_to_SCROBBLINGPAUSED_if_not_stopping_and_not_playing_forward() {
-
+        SongState.SCROBBLING.applyNext(m, update);
+        assertEquals(SongState.SCROBBLINGPAUSED, m.currentState);
     }
 
+    @Test
     public void test_SCROBBLING_to_SCROBBLINGPAUSED_emits_TransitionEvent() {
-
+        assertEquals(TransitionEvent.class, SongState.SCROBBLING.applyNext(m, update).getClass());
     }
 
+    @Test
     public void test_SCROBBLING_emits_no_event_if_playing_forward() {
-
+        when(m.isPlayingForward(update)).thenReturn(true);
+        assertNull(SongState.SCROBBLING.applyNext(m, update));
     }
 
+    @Test
     public void test_SCROBBLINGPAUSED_to_STOPPED_if_song_is_stopping() {
-
+        when(update.isAtEnd()).thenReturn(true);
+        SongState.SCROBBLINGPAUSED.applyNext(m, update);
+        assertEquals(SongState.STOPPED, m.currentState);
     }
 
+    @Test
     public void test_SCROBBLINGPAUSED_to_STOPPED_emits_ScrobbleEvent() {
-
+        when(update.isAtEnd()).thenReturn(true);
+        assertEquals(ScrobbleEvent.class, SongState.SCROBBLINGPAUSED.applyNext(m, update).getClass());
     }
 
+    @Test
     public void test_SCROBBLINGPAUSED_to_SCROBBLING_if_song_is_playing_forward() {
-
+        when(m.isPlayingForward(update)).thenReturn(true);
+        SongState.SCROBBLINGPAUSED.applyNext(m, update);
+        assertEquals(SongState.SCROBBLING, m.currentState);
     }
 
+    @Test
     public void test_SCROBBLINGPAUSED_to_SCROBBLING_emits_TransitionEvent() {
-
+        when(m.isPlayingForward(update)).thenReturn(true);
+        assertEquals(TransitionEvent.class, SongState.SCROBBLINGPAUSED.applyNext(m, update).getClass());
     }
 
+    @Test
     public void test_SCROBBLINGPAUSED_emits_no_event_if_not_stopping_and_not_playing_forward() {
-
+        assertNull(SongState.SCROBBLINGPAUSED.applyNext(m, update));
     }
 }
